0e1d6ccc72a872b73103c74425aeaa7e01ab5143
yonatanaltman <yonatan@yaltman.com>
2023-01-10 09:31:41 +0200

    first commit

---
+++ fitst-api-ever/Program.cs
@@ -0,0 +1,20 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello e-squre");
+
+app.MapGet("/employees", () => "Hello e-squre employees");
+
+var employees = app.MapGroup("employee");
+employees.MapGet("/", () => "Eliran");
+employees.MapPost("/", async (string name) =>
+{
+    string _n = name;
+    Thread.Sleep(2000);
+    return TypedResults.Created($"new employee {_n}");
+});
+
+app.Run();
+
+
